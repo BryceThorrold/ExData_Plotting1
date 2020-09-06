d9189603b13bf02a1d21f29b8f270cc09335cdb1
BryceThorrold <b.thorrold@mweb.co.za>
2020-09-06 21:12:17 +0200

    Got subset of data, and finished question 1

--- plot1.R
+++ plot1.R
@@ -1,5 +1,17 @@
 
 
+####################
+##
+##
+##  16/12/2006;17:24:00;
+##  dd/mm/yyyy
+##
+## Default comes in as yyyy-mm-dd
+## as.Date(powerdata$Date,"%d/%m/%Y"
+##
+
+
+
 if (!file.exists("./powerdata.zip")){
   fileURL = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
   fileName = "household_power_consumption.txt"
@@ -11,12 +23,16 @@ powerdata <- read.table(unz("./powerdata.zip",fileName),
                         sep = ";",
                         stringsAsFactors = FALSE,
                         na.strings = "?",
-##                        colClasses = c("date", "date", rep("numeric",7)),
                         colClasses = c("character", "character", rep("numeric",7)),
                         dec=".")
 
+## mean(powerdata$Voltage, na.rm = TRUE)
 
+## smallertable <- subset(powerdata,as.Date(powerdata$Date,"%d/%m/%Y")=="2007-02-01")
+smallertable <- subset(powerdata,as.Date(powerdata$Date,"%d/%m/%Y")=="2007-02-01" | 
+                         as.Date(powerdata$Date,"%d/%m/%Y")=="2007-02-02")
 
+hist(smallertable$Global_active_power, main = "Global Active Power", col = "red", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
 
 
 
