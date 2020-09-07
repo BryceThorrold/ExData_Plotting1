c09d5074bd9c48e5e1cdf765fb6d0b275ce22764
BryceThorrold <b.thorrold@mweb.co.za>
2020-09-07 22:16:55 +0200

    plot 4 completed

--- plot4.R
+++ plot4.R
@@ -20,33 +20,32 @@ smallertable <- subset(powerdata,as.Date(powerdata$Date,"%d/%m/%Y")=="2007-02-01
 
 smallertable$fulldate <- strptime(paste(smallertable$Date,smallertable$Time),"%d/%m/%Y %H:%M:%S", "")
 
+
+dev.print(png, file = "./plot4.png", width = 480, height = 480)
+png(file = "./plot4.png", bg = "transparent")
+
 par(mfrow = c(2,2))
-plot(smallertable$fulldate,smallertable$Global_active_power, type = "n", ylab = "Active Power")
 
+### Plot 1
+plot(smallertable$fulldate,smallertable$Global_active_power, type = "n", ylab = "Active Power", xlab = "")
+lines(smallertable$fulldate,smallertable$Global_active_power)
 
+## Plot 2
+plot(smallertable$fulldate,smallertable$Voltage, type = "n", ylab = "Voltage", xlab = "datetime")
+lines(smallertable$fulldate,smallertable$Voltage)
 
-plot(smallertable$fulldate,smallertable$Voltage, type = "n", ylab = "Voltage")
-
-
-
-
-plot(smallertable$fulldate,smallertable$Sub_metering_1, type = "n", ylab = "Energy sub metering")
+## Plot 3
+plot(smallertable$fulldate,smallertable$Sub_metering_1, type = "n", ylab = "Energy sub metering", xlab = "")
 lines(smallertable$fulldate,smallertable$Sub_metering_1, col = "grey")
 lines(smallertable$fulldate,smallertable$Sub_metering_2, col = "red")
 lines(smallertable$fulldate,smallertable$Sub_metering_3, col = "blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col=c("grey", "red", "blue"), lty=1, cex=0.8)
+
+## Plot 4
+plot(smallertable$fulldate,smallertable$Global_reactive_power, type = "n", ylab = "Global_reactive_power", xlab = "datetime")
+lines(smallertable$fulldate,smallertable$Global_reactive_power)
 
 
 
-
-plot(smallertable$fulldate,smallertable$Global_reactive_power, type = "n", ylab = "Energy sub metering")
-
-
-##plot(smallertable$fulldate,smallertable$Global_active_power,  type="n", main = "Global Active Power", ylab = "Global Active Power (kilowatts)", xlab = "")
-##lines(smallertable$fulldate,smallertable$Global_active_power)
-
-
-##dev.print(png, file = "./plot3.png", width = 480, height = 480)
-##png(file = "./plot3.png", bg = "transparent")
-##plot(smallertable$fulldate,smallertable$Global_active_power,  type="n", main = "Global Active Power", ylab = "Global Active Power (kilowatts)", xlab = "")
-##lines(smallertable$fulldate,smallertable$Global_active_power)
-##dev.off()
+dev.off()
